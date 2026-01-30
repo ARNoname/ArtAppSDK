74d8d83d379ae0387cf40e2e13f21f379c25f082
NoNmae <arromanyuk.noname@gmail.com>
2026-01-30 11:28:12 +0200

    fix

--- Sources/ArtAppSDK/ArtApps.swift
+++ Sources/ArtAppSDK/ArtApps.swift
@@ -15,7 +15,7 @@ public class ArtApps {
     private var serverRestrictionsUpdatedAt: Date?
     
     // Configurable thresholds (could be remote config in future)
-    public var sessionGateSeconds: TimeInterval =  30//0
+    public var sessionGateSeconds: TimeInterval =  0
     public var frequencyCapSeconds: TimeInterval = 90
     
     public private(set) var serverCooldownSeconds: TimeInterval?
